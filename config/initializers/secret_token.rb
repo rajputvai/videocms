f8aab99d8b4c58d2c10a93a3f51c80936c204a22
Vaibhav Rajput <rajputvai@gmail.com>
2013-09-01 13:01:05 +0530

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videocms::Application.config.secret_key_base = '27f25685de1730bde76bce6af20a01db2e95d29daf126e9657fad24f2b949dac61e9b90db06a68a6a74183871ea52f4b8442942f483586c451871d20460b3cbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Videocms::Application.config.session_store :cookie_store, key: '_videocms_session'
